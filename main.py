85049ee6ef70235e9c5aa9171d2ff7d124e8dc41
threedisk <threedisk@gmail.com>
2020-12-29 11:02:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+line = "================"
+randtoss = random.randint(0,1)
+print (line)
+print (randtoss)
+print (line)
+if randtoss == 1:
+  print ("Heads")
+else:
+  print ("Tails")
+
+# print (randtoss)
+
+
+#List 
+#fruits = ["Apple","Orange","Mango","Peach"]
+
+#print (fruits[1]) will print Orange
+
+#fruits.append("Banana") will append or add item (banana) in list
+#fruits.append("Grape","Pear") will append or add all item (banana) to list
+
+
+
